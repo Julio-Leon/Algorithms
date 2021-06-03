2a9f989e186f2d181546d35f28223ee1d2571b37
julileon10 <julio.leon.diaz1@gmail.com>
2021-06-03 10:41:49 -0400

    Making it shorter

--- largest_number/script.js
+++ largest_number/script.js
@@ -1,4 +1,4 @@
-const largestNumber = num => num > 0? Number("9".repeat(num)): num === 0 ? 0 : Number(`0.${"9".repeat(Math.abs(num))}`);
+const largestNumber = n => n >= 0? Number("9".repeat(n)): Number("0." + "9".repeat(-n));
 // console.log(largestNumber(-9));
 // console.log(largestNumber(0));
-// console.log(largestNumber(5));
+// console.log(largestNumber(15));
